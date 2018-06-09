2bc53e819d5510c6a57259099ef8862366fa35e8
Richard Blondet <richardblondet@gmail.com>
2018-06-09 19:30:52 -0400

    adding menu separate

--- app/Http/Controllers/RolesController.php
+++ app/Http/Controllers/RolesController.php
@@ -14,7 +14,7 @@ class RolesController extends Controller
      */
     public function index()
     {
-        //
+        return view('roles.index');
     }
 
     /**
---
+++ resources/views/shared/menu.blade.php
@@ -0,0 +1,72 @@
+<!-- ### $Sidebar Menu ### -->
+<ul class="sidebar-menu scrollable pos-r">
+	<li class="nav-item dropdown">
+		<a class="dropdown-toggle" href="javascript:void(0);">
+			<span class="icon-holder">
+				<i class="ti-star"></i>
+			</span>
+			<span class="title">Rentas</span>
+			<span class="arrow">
+				<i class="ti-angle-right"></i>
+			</span>
+		</a>
+		<ul class="dropdown-menu">
+			<li>
+				<a class='sidebar-link' href="#">Gestionar Rentas</a>
+			</li>
+			<li>
+				<a class='sidebar-link' href="#">Inspecciones</a>
+			</li>
+			<li>
+				<a class='sidebar-link' href="#">Créditos Clientes</a>
+			</li>
+		</ul>
+	</li>
+	<li class="nav-item dropdown">
+		<a class="dropdown-toggle" href="javascript:void(0);">
+			<span class="icon-holder">
+				<i class="ti-user"></i>
+			</span>
+			<span class="title">Usuarios</span>
+			<span class="arrow">
+				<i class="ti-angle-right"></i>
+			</span>
+		</a>
+		<ul class="dropdown-menu">
+			<li>
+				<a class='sidebar-link' href="#">Gestionar Usuarios</a>
+			</li>
+			<li>
+				<a class='sidebar-link' href="#">Roles</a>
+			</li>
+		</ul>
+	</li>
+	<li class="nav-item dropdown">
+		<a class="dropdown-toggle" href="javascript:void(0);">
+			<span class="icon-holder">
+				<i class="ti-car"></i>
+			</span>
+			<span class="title">Vehículos</span>
+			<span class="arrow">
+				<i class="ti-angle-right"></i>
+			</span>
+		</a>
+		<ul class="dropdown-menu">
+			<li>
+				<a class='sidebar-link' href="#">Gestionar Vehículos</a>
+			</li>
+			<li>
+				<a class='sidebar-link' href="#">Tipos de Vehículos</a>
+			</li>
+			<li>
+				<a class='sidebar-link' href="#">Modelos</a>
+			</li>
+			<li>
+				<a class='sidebar-link' href="#">Marcas</a>
+			</li>
+			<li>
+				<a class='sidebar-link' href="#">Combustibles</a>
+			</li>
+		</ul>
+	</li>
+</ul>
