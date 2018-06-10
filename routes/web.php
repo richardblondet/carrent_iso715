0bec5025f29161da60efae5ac9b1d5b4b7c7e2ff
Richard Blondet <richardblondet@gmail.com>
2018-06-10 16:46:41 -0400

    Adding destroy methods for every controller

--- routes/web.php
+++ routes/web.php
@@ -30,4 +30,12 @@ Route::resources([
 ]);
 
 Route::get('/roles/{id}/delete', 'RolesController@destroy')->name('roles.destroy');
-Route::get('/roles/{id}/delete', 'UsuariosController@destroy')->name('usuarios.destroy');
+Route::get('/usuarios/{id}/delete', 'UsuariosController@destroy')->name('usuarios.destroy');
+Route::get('/creditoclientes/{id}/delete', 'CreditoClientesController@destroy')->name('creditoclientes.destroy');
+Route::get('/tiposvehiculos/{id}/delete', 'TiposVehiculosController@destroy')->name('tiposvehiculos.destroy');
+Route::get('/marcas/{id}/delete', 'MarcasController@destroy')->name('marcas.destroy');
+Route::get('/modelos/{id}/delete', 'ModelosController@destroy')->name('modelos.destroy');
+Route::get('/tiposcombustibles/{id}/delete', 'TiposCombustiblesController@destroy')->name('tiposcombustibles.destroy');
+Route::get('/vehiculos/{id}/delete', 'VehiculosController@destroy')->name('vehiculos.destroy');
+Route::get('/inspeccion/{id}/delete', 'InspeccionController@destroy')->name('inspeccion.destroy');
+Route::get('/renta/{id}/delete', 'RentaController@destroy')->name('renta.destroy');
